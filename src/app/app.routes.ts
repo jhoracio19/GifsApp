839f14f13336f1cf0cb154ef1de0e4914104322d
Jose Horacio <jhoracio19@hotmail.com>
2025-10-22 13:54:19 -0600

    agregando rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./gifs/pages/dashboard-page/dashboard-page'),
+  },
+  {
+    path: 'trending',
+    loadComponent: () =>
+      import('./gifs/pages/dashboard-page/dashboard-page'),
+  },
+  {
+    path: 'search',
+    loadComponent: () =>
+      import('./gifs/pages/dashboard-page/dashboard-page'),
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+
+];
