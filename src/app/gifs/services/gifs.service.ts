4c392764a03af1d791cd878c98986eeb926c771b
Jose Horacio <jhoracio19@hotmail.com>
2025-10-22 22:54:07 -0600

    proyecto terminado

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -21,7 +21,18 @@ export class GifService {
       this.loadTrendingGifs();
     }
     trendingGifs = signal<Gif[]>([]);
-    trendingGifsLoading = signal(true);
+    trendingGifsLoading = signal(false);
+    private trendingPage = signal(0);
+
+    trendingGifGroup = computed<Gif[][]>(() => {
+      const groups = [];
+
+      for(let i=0; i < this.trendingGifs().length; i+=3){
+        groups.push(this.trendingGifs().slice(i, i+3))
+      }
+
+      return groups;
+    })
 
     searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage())
     searchHistoryKeys = computed( () => Object.keys(this.searchHistory()));
@@ -35,15 +46,25 @@ export class GifService {
 
 
   loadTrendingGifs(){
+
+    if(this.trendingGifsLoading()) return;
+
+    this.trendingGifsLoading.set(true);
+
     this.http.get<GiphyReponse>(`${ environment.giphyUrl }/gifs/trending`,{
       params:{
         api_key: environment.giphyApiKey,
         limit: 20,
+        offset: this.trendingPage() * 20,
       }
     })
     .subscribe( (resp) => {
       const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data)
-      this.trendingGifs.set(gifs);
+      this.trendingGifs.update(currentGifs => [
+        ...currentGifs,
+        ...gifs
+      ]);
+      this.trendingPage.update(current => current + 1)
       this.trendingGifsLoading.set(false)
     } )
   }
