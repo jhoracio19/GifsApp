ced90643889a183ea84ea557d88302a6514d43c6
Jose Horacio <jhoracio19@hotmail.com>
2025-10-22 20:02:25 -0600

    agregando buscador de gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -4,6 +4,7 @@ import { environment } from '@environments/environment.development';
 import type { GiphyReponse } from '../interfaces/giphy.interfaces';
 import { Gif } from '../interfaces/gif.interfaces';
 import { GifMapper } from '../mapper/gif.mapper';
+import { map, tap } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class GifService {
@@ -28,5 +29,24 @@ export class GifService {
       this.trendingGifs.set(gifs);
       this.trendingGifsLoading.set(false)
     } )
+  }
+
+  searchGifs(query: string){
+    return this.http.get<GiphyReponse>(`${ environment.giphyUrl }/gifs/search`,{
+      params:{
+        api_key: environment.giphyApiKey,
+        limit: 20,
+        q: query,
+      }
+    })
+    .pipe(
+      map(({data}) => data),
+      map((items) => GifMapper.mapGiphyItemsToGifArray(items)),
+    );
+    // TODO: Historial
+    // .subscribe( (resp) => {
+    //   const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data)
+    //   console.log({search: gifs});
+    // } )
   }
 }
